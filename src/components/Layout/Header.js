fedbbef72a935cbffcd370b71ec009f76e65a923
Ved Jha <vedprakash@fanclash.in>
2023-09-06 23:24:45 +0530

    food order first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, {Fragment} from "react";
+import classes from '../Layout/Header.module.css';
+import foodImg from '../../assets/meals.jpg';
+
+
+const Header = () =>{
+    return(
+        <Fragment>
+            <header className={classes.header}>
+                MealsMELA
+            </header>
+            <div className={classes['main-image']}>
+                <img src={foodImg} alt="Delicious Foods"/>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
+
